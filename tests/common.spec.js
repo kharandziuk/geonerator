78f4b93fe65b28bd3b087de88aa48ca6490dec5a
Max Kharandziuk <max.kharandziuk@here.com>
2017-01-11 16:16:23 +0100

    initial implementation of a bounding box generator

--- tests/common.spec.js
+++ tests/common.spec.js
@@ -1,33 +1,76 @@
 'use strict';
 const geolib = require('geolib');
 const should = require('chai').expect;
+const _ = require('lodash');
 
-function generate(center, radius) {
-  const topLeft = geolib.computeDestinationPoint(
-    center, radius, 360 - 45
+const DEFAULT_DISTANCE_IN_KM = 5
+
+function generate(center, radius = DEFAULT_DISTANCE_IN_KM) {
+  const names = [
+      'topLeft', 'topRight',
+      'bottomRight', 'bottomLeft',
+  ]
+  const pairs = _(names).map((n, i) => [n, i * 90 + 45]).value()
+  const verticesByName = _(pairs).reduce(
+    function(obj, [name, rotation]) {
+      obj[name] = geolib.computeDestinationPoint(
+        center, radius, rotation
+      )
+      return obj
+    },
+    {}
   )
-  const rightBottom = geolib.computeDestinationPoint(
-    center, radius, 360 - 45
-  )
-  return {center, rightBottom, topLeft}
+  const vertices = _(names).map(name => verticesByName[name]).value()
+  return _.extend(verticesByName, {vertices, center})
 }
+
 const point = (latitude, longitude) => ({latitude, longitude})
 
-describe.only('bounding box', function() {
-  it('can generate bounding box with center and radius', function() {
-    should(generate(point(52, 13), 5)).eql({
-      center: { latitude: 52, longitude: 13 },
-      rightBottom: { latitude: 52.000031760230186, longitude: 12.999948412780023 },
-      topLeft: { latitude: 52.000031760230186, longitude: 12.999948412780023 }
-    })
+describe('bounding box', function() {
 
+  it('can generate random bounding box with a default radius', function(){
+    const bbox = generate()
+    should.fail()
   })
-  it('can generate bounding box with center and zero radius', function() {
+
+  it('can generate bounding box with center and radius', function() {
+    const RADIUS = 5
+    const center = point(52, 13)
+    const bbox = generate(center, RADIUS)
+    _(bbox.vertices).each(function(v) {
+      should(geolib.getDistance(center, v)).equal(RADIUS)
+    })
+    should(bbox).eql({
+      center: { latitude: 52, longitude: 13 },
+      topLeft: { latitude: 52.000031760230186, longitude: 13.000051587219987 },
+      topRight: { latitude: 51.99996823974729, longitude: 13.00005158714681 },
+      bottomRight: { latitude: 51.99996823974729, longitude: 12.999948412853202 },
+      bottomLeft: { latitude: 52.000031760230186, longitude: 12.999948412780023 },
+      vertices: [
+        { latitude: 52.000031760230186, longitude: 13.000051587219987 },
+        { latitude: 51.99996823974729, longitude: 13.00005158714681 },
+        { latitude: 51.99996823974729, longitude: 12.999948412853202 },
+        { latitude: 52.000031760230186, longitude: 12.999948412780023 } 
+      ]
+    })
+  })
+
+  xit('can generate bounding box with center and zero radius', function() {
     should(generate(point(52, 13), 0)).eql({
       center: { latitude: 52, longitude: 13 },
       rightBottom: { latitude: 52.00000000000001, longitude: 13.000000000000005 },
       topLeft: { latitude: 52.00000000000001, longitude: 13.000000000000005 }
     })
+  })
+
+  xit('can generate a point inside of bounding box', function() {
+    const bbox = generate()
+    const pointInside = bbox.generatePoint()
+    const isPointInside = geolib.isPointInside(point, [
+      bbox.topLeft, bbox.topRight,
+      bbox.bottomRight, bbox.bottomLeft,
+    ])
+    should(isPointInside).to.be.true()
   })
 })
 
