576eded745147e16192c023fe13b93ecaccd9413
Max Kharandziuk <max.kharandziuk@here.com>
2017-01-11 16:52:47 +0100

    move the library code from tests to a separate file

--- tests/common.spec.js
+++ tests/common.spec.js
@@ -1,28 +1,9 @@
 'use strict';
+const _ = require('lodash');
 const geolib = require('geolib');
 const should = require('chai').expect;
-const _ = require('lodash');
 
-const DEFAULT_DISTANCE_IN_KM = 5
-
-function generate(center, radius = DEFAULT_DISTANCE_IN_KM) {
-  const names = [
-      'topLeft', 'topRight',
-      'bottomRight', 'bottomLeft',
-  ]
-  const pairs = _(names).map((n, i) => [n, i * 90 + 45]).value()
-  const verticesByName = _(pairs).reduce(
-    function(obj, [name, rotation]) {
-      obj[name] = geolib.computeDestinationPoint(
-        center, radius, rotation
-      )
-      return obj
-    },
-    {}
-  )
-  const vertices = _(names).map(name => verticesByName[name]).value()
-  return _.extend(verticesByName, {vertices, center})
-}
+const generate = require('../lib/geonerator')
 
 const point = (latitude, longitude) => ({latitude, longitude})
 
